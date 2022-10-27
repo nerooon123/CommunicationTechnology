6ecc963b62153b371c998c6795ce43381f8e2854
Timur <115815998+nerooon123@users.noreply.github.com>
2022-10-27 08:36:18 +0500

    abc
    abc

---
+++ StringCheckLibrary/CheckStringClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCheckLibrary
+{
+    public class CheckStringClass
+    {
+        public bool LoginCheck(string login)
+        {
+            string correctSymbols = "abcdefghijklmnopqrstuvwxyz0123456789-_.";
+            login = login.ToLower();
+            if (!login.All(x => correctSymbols.Contains(x)))
+            {
+                throw new Exception("Логин содержит недопустимые символы");
+            }
+            if (login == String.Empty)
+            {
+                throw new Exception("Вы не ввели логи");
+            }
+            if (login.EndsWith("."))
+            {
+                throw new Exception("Логин не может заканчиваться символом точка");
+            }
+            return true;
+        }
+    }
+}
